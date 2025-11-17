94e07b68247589d62bf046571a4281742a71b43e
Ricardo <rleo.9016@gmail.com>
2025-11-17 14:58:47 -0500

    Scripts catalogo actualizados al repositorio

---
+++ scripts/06. spCrearCatalogo.sql
@@ -0,0 +1,29 @@
+USE TestifyDB
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+Create PROCEDURE dbo.spCrearCatalogo
+@tipCatId bigint,
+@catNombre varchar(200),
+@catDescripcion varchar(200),
+@catEstado char(1),
+@usuIdReg int
+AS        
+BEGIN      
+	insert into Catalogo(
+		tipCatId,
+		catNombre,
+		catDescripcion,
+		catEstado,
+		catFechaReg,
+		usuIdReg)
+	values(
+		@tipCatId,
+		@catNombre,
+		@catDescripcion,
+		@catEstado,
+		GETDATE(),
+		@usuIdReg)
+END
