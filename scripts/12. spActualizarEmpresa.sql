8d97daf7d8dffc6312b14136b784926b62669cc5
Ricardo <rleo.9016@gmail.com>
2025-11-27 13:57:38 -0500

    Actualización de sp

---
+++ scripts/12. spActualizarEmpresa.sql
@@ -0,0 +1,30 @@
+USE TestifyDB
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+Create PROCEDURE dbo.spActualizarEmpresa
+@empId bigint,
+@empNombre varchar(150),
+@empRuc varchar(13),
+@empDireccion varchar(200),
+@empEstado smallint,
+@usuIdReg int
+AS        
+BEGIN      
+	update Empresa
+	set
+		empNombre=@empNombre,
+		empRuc=@empRuc,
+		empDireccion=@empDireccion,
+		empEstado=@empEstado,
+		usuIdAct=@usuIdReg,
+		usuFechaReg=SYSDATETIME()
+	where empId=@empId
+
+	SELECT @@ROWCOUNT AS RowsAffected;
+END
+
+
+
---
+++ scripts/13. spEliminarEmpresa.sql
@@ -0,0 +1,22 @@
+USE TestifyDB
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+Create PROCEDURE dbo.spEliminarEmpresa
+@empId bigint,
+@usuIdReg int
+AS        
+BEGIN      
+	update Empresa
+	set
+		empEstado=2,
+		usuIdEli=@usuIdReg,
+		usuFechaEli=SYSDATETIME()
+	where empId=@empId
+
+	SELECT @@ROWCOUNT AS RowsAffected;
+END
+
+
